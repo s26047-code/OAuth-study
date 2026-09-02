36bbd952fb37278864278af8ee76bd9c7afeb48c
이다원 <s26047@gsm.hs.kr>
2026-09-02 09:05:02 +0900

    수정 내용 반영

--- src/main/java/com/oauthstudy/domain/entity/RefreshToken.java
+++ src/main/java/com/oauthstudy/domain/entity/RefreshToken.java
@@ -1,6 +1,11 @@
 package com.oauthstudy.domain.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +16,13 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "refresh_token")
 public class RefreshToken {
 
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private static final long REFRESH_TOKEN_EXPIRATION_DAYS = 7;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
@@ -24,18 +33,20 @@ public class RefreshToken {
     private LocalDateTime expiryDate;
 
     @Builder
-    public RefreshToken(String token, Long userId, LocalDateTime expiryDate) {
+    public RefreshToken(String token, Long userId) {
         this.token = token;
         this.userId = userId;
-        this.expiryDate = expiryDate;
+        this.expiryDate = LocalDateTime.now()
+                .plusDays(REFRESH_TOKEN_EXPIRATION_DAYS); //자동 계산 로직
     }
 
     public boolean isExpired() {
         return expiryDate.isBefore(LocalDateTime.now());
     }
 
-    public void updateToken(String newtoken, LocalDateTime newExpirDate){ //토큰 재발금
-        this.token = newtoken;
-        this.expiryDate = newExpirDate;
+    public void updateToken(String newToken) {
+        this.token = newToken;
+        this.expiryDate = LocalDateTime.now()
+                .plusDays(REFRESH_TOKEN_EXPIRATION_DAYS);
     }
 }
